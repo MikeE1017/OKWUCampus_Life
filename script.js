3654a8057ccd6807173149711d0ee05f4f0ffb81
MikeE1017 <mjelovesmge.101706@gmail.com>
2025-11-15 14:15:17 -0600

    MVP code modification and css styling

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ Home | Map | Dining | Events | Community | Devotions | Schedule
 document.getElementById("loadScheduleBtn").addEventListener("click", () => {
     alert("This feature will load your class schedule soon!");
 });
+
 const diningAPI = "https://api.placeholder.com/menu"; // future API placeholder
+
